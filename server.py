e4d6fd0cab66e84f5965fdb258381c5fcf3bbfb4
adeykin <adeykin90@gmail.com>
2020-04-11 16:45:49 +0300

    Base impl of connection by web socket

---
+++ server.py
@@ -0,0 +1,38 @@
+import asyncio
+import websockets
+
+USERS = set()
+
+async def register(websocket):
+    USERS.add(websocket)
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+
+async def notify_users(message):
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def notify_users_except_me(websocket, message):
+    users_without_me = set(filter(lambda x: x != websocket,USERS))
+    if users_without_me:
+        await asyncio.wait([user.send(message) for user in users_without_me])
+        
+
+async def hello2(websocket, path):
+    await register(websocket)
+    try:
+        async for message in websocket:
+            print(message)
+            await notify_users_except_me(websocket, message)
+            #await websocket.send(message)
+            
+    finally:
+        await unregister(websocket)
+
+#start_server = websockets.serve(hello2, "localhost", 6789) #8765
+start_server = websockets.serve(hello2, "192.168.1.64", 6789) #8765
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
